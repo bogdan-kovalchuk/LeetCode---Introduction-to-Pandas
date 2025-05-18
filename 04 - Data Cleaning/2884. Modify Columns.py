58eef6afd2cf891921fb198ed1e60206cb0941b0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-18 07:38:44 +0300

    2884 solution

--- 04 - Data Cleaning/2884. Modify Columns.py
+++ 04 - Data Cleaning/2884. Modify Columns.py
@@ -1,2 +1,11 @@
-# 2884
-# Modify Columns
+import pandas as pd
+
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    # Option 1
+    employees["salary"] = employees["salary"] * 2
+
+    # Option 2
+    employees.salary *= 2
+
+    return employees
