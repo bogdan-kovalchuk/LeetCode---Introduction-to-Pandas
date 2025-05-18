06ac0e1d41916e1c44373a1c5856bf92c00a430a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-18 20:03:18 +0300

    2890 solution

--- 05 - Table Reshaping/2890. Reshape Data_ Melt.py
+++ 05 - Table Reshaping/2890. Reshape Data_ Melt.py
@@ -1,2 +1,10 @@
-# 2890
-# Reshape Data: Melt
+import pandas as pd
+
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(
+        id_vars="product",
+        value_vars=["quarter_1", "quarter_2", "quarter_3", "quarter_4"],
+        var_name="quarter",
+        value_name="sales",
+    )
