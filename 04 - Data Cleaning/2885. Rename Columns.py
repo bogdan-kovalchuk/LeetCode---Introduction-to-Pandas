8fd9583fec5160ff96f6c236ad0fe0c1f65934e0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-18 07:49:32 +0300

    2885 solution

--- 04 - Data Cleaning/2885. Rename Columns.py
+++ 04 - Data Cleaning/2885. Rename Columns.py
@@ -1,2 +1,18 @@
-# 2885
-# Rename Columns
+import pandas as pd
+
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    # Option 1
+    students.columns = ["student_id", "first_name", "last_name", "age_in_years"]
+
+    # Option 2
+    students.rename(
+        columns={
+            "id": "student_id",
+            "first": "first_name",
+            "last": "last_name",
+            "age": "age_in_years",
+        },
+        inplace=True,
+    )
+    return students
